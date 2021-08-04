8c899820c2e12869dab64500b850caa19085b687
Fourier Transformer <konykony22@gmail.com>
2021-08-04 19:09:20 +0200

    ydnjs hard binding

--- engine.js
+++ engine.js
@@ -1,8 +1,12 @@
-for(var i=0;i<10;i++)
-{
-    alert("warning"+i);
-}
-//ddddddddddd
-alert("branch_A ");
-alert("graph !!");
-//dddd
+function foo(something) {
+    console.log( this.a, something );
+    return this.a + something;
+    }
+    var obj = {
+    a: 2
+    };
+    var bar = function() {
+    return foo.apply( obj, arguments );
+    };
+    var b = bar( 3 ); // 2 3
+    console.log( b );
