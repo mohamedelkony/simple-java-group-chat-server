14554465f0d87da4be83899820b85d11ef4c26b9
Fourier Transformer <konykony22@gmail.com>
2021-08-16 01:37:09 +0200

    ddddddddd

--- engine.js
+++ engine.js
@@ -1,12 +1,7 @@
-function foo(something) {
-    console.log( this.a, something );
-    return this.a + something;
-    }
-    var obj = {
-    a: 2
+var A={
+    x:3,
+    y:4
     };
-    var bar = function() {
-    return foo.apply( obj, arguments );
-    };
-    var b = bar( 3 ); // 2 3
-    console.log( b );
+    a1=Object.create(A);
+    console.log(a1.x);
+    
